561291ab933ef7646cfaf5ed5c4aed97f27f6a2d
Aman Adhikari <amanadhikari2@gmail.com>
2018-05-23 23:45:32 +0530

    Added Few things in user

--- app/src/main/java/com/recom/www/recommenderapp/Models/User_Model.java
+++ app/src/main/java/com/recom/www/recommenderapp/Models/User_Model.java
@@ -3,20 +3,50 @@ package com.recom.www.recommenderapp.Models;
 import android.databinding.BaseObservable;
 
 public class User_Model extends BaseObservable {
-    private String title, genre, name,price,imgUrl,mile;
-    private int rating;
+    private String date, time, name,thumb;
+    private float rating;
 
     public User_Model() {
     }
 
-    public User_Model(String title, String genre, String name,String price,int rating,String imgUrl,String mile) {
-        this.title = title;
-        this.genre = genre;
-        this.name = name;
-        this.price=price;
+    public User_Model(String date, String time, String name,float rating) {
+        this.date=date;
+        this.name=name;
         this.rating=rating;
-        this.imgUrl=imgUrl;
-        this.mile=mile;
+        this.time=time;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
