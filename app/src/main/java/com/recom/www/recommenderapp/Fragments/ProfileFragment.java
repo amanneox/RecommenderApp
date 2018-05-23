b0c7677907af440c6aa5c47cf26b2e3d7e1ffdc3
Aman Adhikari <amanadhikari2@gmail.com>
2018-05-23 22:27:14 +0530

    Added Few things in user

--- app/src/main/java/com/recom/www/recommenderapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/recom/www/recommenderapp/Fragments/ProfileFragment.java
@@ -3,6 +3,9 @@ package com.recom.www.recommenderapp.Fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +14,20 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 import com.recom.www.recommenderapp.Adapters.CustomGridViewActivity;
+import com.recom.www.recommenderapp.Adapters.HomeAdapter;
+import com.recom.www.recommenderapp.Adapters.UserAdapter;
+import com.recom.www.recommenderapp.Models.Home_Model;
+import com.recom.www.recommenderapp.Models.User_Model;
 import com.recom.www.recommenderapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
     GridView androidGridView;
+    private List<User_Model> itemlist = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private UserAdapter mAdapter;
     String[] gridViewString = {
             "Bookmarks", "My Reviews", "My Check-ins", "Settings", "Bars", "Coffee",
             "Gas Station"
@@ -30,7 +43,15 @@ public class ProfileFragment extends Fragment {
 
 
         View rootview = inflater.inflate(R.layout.user_view, container, false);
+        RecyclerView recyclerView = (RecyclerView)rootview.findViewById(R.id.recycler_view_user);
 
+        mAdapter = new UserAdapter(itemlist);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareUserData();
         CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(getContext(), gridViewString, gridViewImageId);
         androidGridView=rootview.findViewById(R.id.options);
         androidGridView.setAdapter(adapterViewAndroid);
@@ -43,5 +64,20 @@ public class ProfileFragment extends Fragment {
             }
         });
         return rootview;
+    }
+
+    private void prepareUserData() {
+        User_Model item = new User_Model("23 May","19:08","Burger King",3.5f);
+        itemlist.add(item);
+        item = new User_Model("23 May","19:08","Burger King",3.5f);
+        itemlist.add(item);
+        item = new User_Model("23 May","19:08","Burger King",3.5f);
+        itemlist.add(item);
+        item = new User_Model("23 May","19:08","Burger King",3.5f);
+        itemlist.add(item);
+        item = new User_Model("23 May","19:08","Burger King",3.5f);
+        itemlist.add(item);
+        item = new User_Model("23 May","19:08","Burger King",3.5f);
+        itemlist.add(item);
     }
 }
