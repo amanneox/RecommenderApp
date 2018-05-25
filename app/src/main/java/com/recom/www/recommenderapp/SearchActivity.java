aa774cd90e1bb0cd1ffb1a2dc0ba21c5ed8bb0a9
Aman Adhikari <amanadhikari2@gmail.com>
2018-05-25 06:49:20 +0530

    Rating bars in home

---
+++ app/src/main/java/com/recom/www/recommenderapp/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.recom.www.recommenderapp;
+
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_view);
+
+    }
+}
