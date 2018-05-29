426111d48329f303c225f8b43d8d805cd4f0c7b3
Aman Adhikari <amanadhikari2@gmail.com>
2018-05-29 20:12:39 +0530

    Fixed Backtracking

--- app/src/main/java/com/recom/www/recommenderapp/DisplayActivity.java
+++ app/src/main/java/com/recom/www/recommenderapp/DisplayActivity.java
@@ -73,7 +73,7 @@ DisplayActivity extends AppCompatActivity {
         // load fragment
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_container, fragment);
-        transaction.addToBackStack(null);
+        //transaction.addToBackStack(null);
         transaction.commit();
     }
 }
